eb589137c3bca1002dbcb57581ad409bd15493f2
judah <87juda@gmail.com>
2022-03-23 23:31:39 -0300

    Add modules folder

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: 'development',
   entry: {
     index: './src/index.js',
-    print: './src/print.js',
+    another: './src/modules/another-module.js',
   },
   devtool: 'inline-source-map',
   devServer: {
