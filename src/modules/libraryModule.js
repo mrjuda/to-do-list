d2904daf3c79dea409fa41a2d7dd363720a2a75b
judah <87juda@gmail.com>
2022-03-26 19:46:02 -0300

    Preserve data in the browser

--- src/modules/libraryModule.js
+++ src/modules/libraryModule.js
@@ -16,7 +16,7 @@ export default class Library {
 
   static pushToStorage = (obj) => {
     const stringify = JSON.stringify(obj);
-    sessionStorage.setItem('strShelf', stringify);
+    localStorage.setItem('strShelf', stringify);
   }
 
   static displayBook(title, id) {
@@ -28,18 +28,35 @@ export default class Library {
   }
 
   pullFromStorage = () => {
-    const parsed = JSON.parse(sessionStorage.getItem('strShelf'));
+    const parsed = JSON.parse(localStorage.getItem('strShelf'));
     let counter = 1;
     const preShelf = [];
-    for (let i = 0; i < this.shelf.length; i += 1) {
+    // Returns the length of the localstorage shelf
+    // (even when the page reloads)
+    const shelfSize = Object.values(parsed).length;
+
+    for (let i = 0; i < shelfSize; i += 1) {
+      // Returns the Nth item saved on the localstorage
+      console.log('LINE 39: shelf.toString(): ' + this.shelf.toString() + '\n pllFrmStrg > shelf = ' + this.shelf);
+      console.log('pllFrmStrg > preShelf = ' + this.preShelf);
+      if (this.shelf.length === 0) {
+        this.shelf.push(Object.values(parsed)[i]);
+      }
+      setTimeout(() => { console.log('LINE 43: pllFrmStrg > for... i = ' + i); }, 500);
       const parsedBook = parsed[`${counter}`];
       const tempShelf = Library.displayBook(parsedBook.title, counter);
-      preShelf.unshift(tempShelf);
+      preShelf.push(tempShelf);
       counter += 1;
     }
+    // console.log('pllFrmStrg > shelf = ' + this.shelf + '\n pllFrmStrg > preShelf = ' + this.preShelf);
+    // console.log('pllFrmStrg > preShelf.length = ' + this.preShelf.length);
+    return preShelf;
+  }
 
+  paintToPage = (caller) => {
+    console.log('caller: ' + caller + ' > paintToPage');
+    const preShelf = this.pullFromStorage();
     this.frontShelf.innerHTML = '';
-
     for (let i = 0; i < preShelf.length; i += 1) {
       const createdBook = document.createElement('div');
       createdBook.classList.add('unchecked-slot', 'book');
@@ -47,19 +64,24 @@ export default class Library {
       createdBook.id = i + 1;
       this.frontShelf.appendChild(createdBook);
     }
+    this.setRemoveListeners();
   }
 
   updateShelf = () => {
+    // console.log('bookshelf A: ' + this.bookshelf);
     this.bookshelf = new StrShelf();
+
     let counter = 0;
     for (let i = 0; i < this.shelf.length; i += 1) {
       counter += 1;
       this.shelf[i].id = counter;
+      // this.shelf[i].index = counter;
     }
     for (let i = 0; i < this.shelf.length; i += 1) {
       // GIVES THE BOOK OBJ AN ID #NUMBER
       this.bookshelf[`${this.shelf[i].id}`] = this.shelf[i];
     }
+    // console.log('bookshelf C: ' + this.bookshelf);
     Library.pushToStorage(this.bookshelf);
   }
 
@@ -67,27 +89,35 @@ export default class Library {
     this.bookshelf = new StrShelf();
     const book = new Book();
     book.title = title;
-    this.shelf.unshift(book);
+    book.completed = false;
+    this.shelf.push(book);
     this.updateShelf();
+    this.paintToPage();
   }
 
   removeBook = (id) => {
-    this.bookshelf = new StrShelf();
+    // this.bookshelf = new StrShelf();
 
-    this.frontShelf.innerHTML = '';
-    this.removeButton = document.querySelectorAll('.removeButton');
-    for (let i = 1; i <= this.shelf.length; i += 1) {
-      if (this.shelf[i - 1].id === parseInt(id, 10)) {
-        this.shelf.splice(i - 1, 1);
+    // this.frontShelf.innerHTML = '';
+    // this.removeButton = document.querySelectorAll('.removeButton');
+
+    for (let i = 0; i < this.shelf.length; i += 1) {
+      console.log('removeBook > shelf.length: ' + this.shelf.length);
+      console.log('removeBook: shelf: ' + this.shelf[0]);
+      if (this.shelf[i].id === parseInt(id, 10)) {
+        this.shelf.splice(i, 1);
         this.updateShelf();
       }
-      this.pullFromStorage();
-      this.removeButton = document.querySelectorAll('.removeButton');
-      this.removeButton.forEach((button) => {
-        button.addEventListener('click', (e) => {
-          this.removeBook(e.target.id);
-        });
-      });
+
+      this.paintToPage();
+      // this.setRemoveListeners();
+      // this.removeButton = document.querySelectorAll('.removeButton');
+      // this.removeButton.forEach((button) => {
+      //   button.addEventListener('click', (e) => {
+      //     console.log('addevent-AA-INSIDEfor-loop');
+      //     this.removeBook(e.target.id);
+      //   });
+      // });
     }
   }
 
@@ -95,18 +125,19 @@ export default class Library {
     this.removeButton = document.querySelectorAll('.removeButton');
     this.removeButton.forEach((button) => {
       button.addEventListener('click', (e) => {
+        // console.log('addevent-AA-OUTSIDE-for-loop');
+        console.log(e.target.id);
         this.removeBook(e.target.id);
       });
     });
   }
 
   setUpAddListener = () => {
-    this.addButton.addEventListener('click', (e) => {
-      e.preventDefault();
+    this.addButton.addEventListener('click', () => {
       this.newBook(this.newTitle.value);
       this.pullFromStorage();
       this.newTitle.value = '';
-      this.setRemoveListeners();
+      // this.setRemoveListeners();
     });
   }
 }
