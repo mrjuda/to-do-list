c9dd20ebb9fe9ccdbf1438f82d051409ec105b31
judah <87juda@gmail.com>
2022-03-26 02:41:09 -0300

    Add to-do list's basic structure

--- another.bundle.js
+++ another.bundle.js
@@ -1,18 +1,15 @@
-"use strict";
 (self["webpackChunkto_do_list"] = self["webpackChunkto_do_list"] || []).push([["another"],{
 
 /***/ "./src/modules/another-module.js":
 /*!***************************************!*\
   !*** ./src/modules/another-module.js ***!
   \***************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (() => {
 
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
-/* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_0__);
+// import _ from 'lodash';
 
+// console.log(_.join(['Another', 'module', 'loaded!'], ' '));
 
-console.log(lodash__WEBPACK_IMPORTED_MODULE_0___default().join(['Another', 'module', 'loaded!'], ' '));
 
 /***/ })
 
@@ -22,4 +19,4 @@ console.log(lodash__WEBPACK_IMPORTED_MODULE_0___default().join(['Another', 'modu
 /******/ var __webpack_exports__ = (__webpack_exec__("./src/modules/another-module.js"));
 /******/ }
 ]);
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYW5vdGhlci5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7O0FBQXVCOztBQUV2QixZQUFZLGtEQUFNIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG8tZG8tbGlzdC8uL3NyYy9tb2R1bGVzL2Fub3RoZXItbW9kdWxlLmpzIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCBfIGZyb20gJ2xvZGFzaCc7XG5cbmNvbnNvbGUubG9nKF8uam9pbihbJ0Fub3RoZXInLCAnbW9kdWxlJywgJ2xvYWRlZCEnXSwgJyAnKSk7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYW5vdGhlci5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7QUFBQTs7QUFFQSIsInNvdXJjZXMiOlsid2VicGFjazovL3RvLWRvLWxpc3QvLi9zcmMvbW9kdWxlcy9hbm90aGVyLW1vZHVsZS5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyBpbXBvcnQgXyBmcm9tICdsb2Rhc2gnO1xuXG4vLyBjb25zb2xlLmxvZyhfLmpvaW4oWydBbm90aGVyJywgJ21vZHVsZScsICdsb2FkZWQhJ10sICcgJykpO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
