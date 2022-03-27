b173437ebad991d827da230985bc97d89f5b026a
judah <87juda@gmail.com>
2022-03-26 23:42:36 -0300

    Add check/uncheck function

--- src/modules/libraryModule.js
+++ src/modules/libraryModule.js
@@ -14,6 +14,10 @@ export default class Library {
 
   removeButton;
 
+  inputField;
+
+  checkboxButton;
+
   static pushToStorage = (obj) => {
     const stringify = JSON.stringify(obj);
     localStorage.setItem('strShelf', stringify);
@@ -21,9 +25,17 @@ export default class Library {
 
   static displayBook(title, id) {
     return `
-        <div class="box unchecked"></div>
-        <div class="task title-author">${title}</div>
-        <div class="handle removeButton" id="${id}" type="button"></div>
+    <div class="box unchecked"></div>
+    <input class="task" type="text" value="${title}" name="Item1">
+    <div class="handle removeButton" id="${id}" type="button"></div>
+        `;
+  }
+
+  static displayCheckedBook(title, id) {
+    return `
+      <div class="box checked"></div>
+      <input class="task" type="text" value="${title}" name="Item1">
+      <div class="handle removeButton" id="${id}" type="button"></div>
         `;
   }
 
@@ -42,8 +54,12 @@ export default class Library {
           this.shelf.push(Object.values(parsed)[i]);
         }
       }
+      //Get the completed value of parsed book
       const parsedBook = parsed[`${counter}`];
-      const tempShelf = Library.displayBook(parsedBook.title, counter);
+      let tempShelf = Library.displayBook(parsedBook.title, counter);
+      if (parsedBook.completed) {
+        tempShelf = Library.displayCheckedBook(parsedBook.title, counter);
+      }
       preShelf.push(tempShelf);
       counter += 1;
     }
@@ -64,17 +80,20 @@ export default class Library {
   }
 
   updateShelf = () => {
+    // console.log('bookshelf A: ' + this.bookshelf);
     this.bookshelf = new StrShelf();
 
     let counter = 0;
     for (let i = 0; i < this.shelf.length; i += 1) {
       counter += 1;
       this.shelf[i].id = counter;
+      // this.shelf[i].index = counter;
     }
     for (let i = 0; i < this.shelf.length; i += 1) {
       // GIVES THE BOOK OBJ AN ID #NUMBER
       this.bookshelf[`${this.shelf[i].id}`] = this.shelf[i];
     }
+    // console.log('bookshelf C: ' + this.bookshelf);
     Library.pushToStorage(this.bookshelf);
   }
 
@@ -86,6 +105,8 @@ export default class Library {
     this.shelf.push(book);
     this.updateShelf();
     this.paintToPage();
+    this.setInputFieldListeners();
+    this.setCheckboxFieldListeners();
   }
 
   removeBook = (id) => {
@@ -95,6 +116,25 @@ export default class Library {
         this.updateShelf();
       }
       this.paintToPage();
+      this.setCheckboxFieldListeners();
+    }
+  }
+
+  updateTask = (id, text) => {
+    for (let i = 0; i < this.shelf.length; i += 1) {
+      if (this.shelf[i].id === parseInt(id, 10)) {
+        this.shelf[i].title = text;
+        this.updateShelf();
+      }
+    }
+  }
+
+  updateCompletion = (id, completed) => {
+    for (let i = 0; i < this.shelf.length; i += 1) {
+      if (this.shelf[i].id === parseInt(id, 10)) {
+        this.shelf[i].completed = completed;
+        this.updateShelf();
+      }
     }
   }
 
@@ -102,7 +142,6 @@ export default class Library {
     this.removeButton = document.querySelectorAll('.removeButton');
     this.removeButton.forEach((button) => {
       button.addEventListener('click', (e) => {
-        console.log(e.target.id);
         this.removeBook(e.target.id);
       });
     });
@@ -113,7 +152,35 @@ export default class Library {
       this.newBook(this.newTitle.value);
       this.pullFromStorage();
       this.newTitle.value = '';
-      // this.setRemoveListeners();
+    });
+  }
+
+  setInputFieldListeners = () => {
+    this.inputField = document.querySelectorAll('.task');
+    this.inputField.forEach((input) => {
+      input.addEventListener('input', (e) => {
+        const text = e.target.value;
+        this.updateTask(e.target.parentElement.id, text);
+      });
+    });
+  }
+
+  setCheckboxFieldListeners = () => {
+    this.checkboxButton = document.querySelectorAll('.box');
+    this.checkboxButton.forEach((checkbox) => {
+      checkbox.addEventListener('click', (e) => {
+        let taskChecked = false;
+        if (e.target.classList.contains('unchecked')) {
+          taskChecked = true;
+          e.target.classList.toggle('checked');
+          e.target.classList.toggle('unchecked');
+        } else if (e.target.classList.contains('checked')) {
+          taskChecked = false;
+          e.target.classList.toggle('checked');
+          e.target.classList.toggle('unchecked');
+        }
+        this.updateCompletion(e.target.parentElement.id, taskChecked);
+      });
     });
   }
 }
